a427264bcb1016326f8df495a4e67f0b3c659131
BengBu-YueZhang <zhangyue_coder@163.com>
2020-01-22 21:52:31 +0800

    更新模版

--- src/news.ts
+++ src/news.ts
@@ -1,22 +1,40 @@
 import * as vscode from 'vscode';
-import { jsonp } from './util';
+import {
+    jsonp,
+    handleError
+} from './util';
 import * as pug from 'pug';
 import * as path from 'path';
 
 export type NewsRange = 'IGN' | 'GameSpot' | '游民星空' | 'All';
 
-export default function news (
+export default async function news (
     context: vscode.ExtensionContext,
     viewType: string,
     title: string,
     newRange: NewsRange
 ) {
-    const panel = vscode.window.createWebviewPanel(
-        viewType,
-        title,
-        vscode.ViewColumn.One,
-        {}
-    );
-    const views = path.resolve(__dirname, './../views/index.htm');
-    panel.webview.html = pug.renderFile(views);
+    try {
+        await vscode.window.withProgress({
+            title: '🎮游戏新闻加载中',
+            location: vscode.ProgressLocation.Notification
+        }, async () => {
+        });
+        const webviewDir = path.resolve(__dirname, './../views');
+        const panel = vscode.window.createWebviewPanel(
+            viewType,
+            title,
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true,
+                retainContextWhenHidden: true,
+                localResourceRoots: [vscode.Uri.file(path.resolve(__dirname, webviewDir))]
+            }
+        );
+        const tpl = path.resolve(__dirname, './../views/index.pug');
+        panel.webview.html = pug.renderFile(tpl, {
+        }); 
+    } catch (error) {
+        handleError(error, title);
+    }   
 }
--- src/util.ts
+++ src/util.ts
@@ -1,2 +1,16 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as pug from 'pug';
+
 export function jsonp () {
+}
+
+export function handleError (error: any, title: string) {
+    const panel = vscode.window.createWebviewPanel(
+        'error',
+        title,
+        vscode.ViewColumn.One
+    );
+    const tpl = path.resolve(__dirname, './../views/error.pug');
+    panel.webview.html = pug.renderFile(tpl); 
 }
