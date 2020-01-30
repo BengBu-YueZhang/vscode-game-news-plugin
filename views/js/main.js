77cb4d213069ec48d5e4e9416f8bc52c38a3709f
BengBu-YueZhang <zhangyue_coder@163.com>
2020-01-30 21:36:26 +0800

    添加样式

--- views/js/main.js
+++ views/js/main.js
@@ -2,5 +2,23 @@ const app = new Vue({
     el: '#app',
     data: {
         news: JSON.parse(GLOBAL_DATA.replace(/&quot;/gi, '"')).news
+    },
+    filters: {
+        cn (title) {
+            switch (title) {
+                case 'Gcores':
+                    return '机核网'
+                case 'Gamersky':
+                    return '游民星空'
+                case 'IGN':
+                    return 'IGN中国'
+                default:
+                    return title
+            }
+        }
+    },
+    methods: {
+        handleCopyLink (link) {
+        }
     }
 })
