a635c6c1b02a89ad432a75b3087abd44afe0aad7
BengBu-YueZhang <zhangyue_coder@163.com>
2020-01-31 19:08:12 +0800

    debug

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import news from './news';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const IGN = vscode.commands.registerCommand('extension.IGN', () => {
+	const IGN = vscode.commands.registerCommand('game.IGN', () => {
 		news(context, 'ign', 'IGN', 'IGN');
 	});
 
-	const 机核网 = vscode.commands.registerCommand('extension.机核网', () => {
+	const 机核网 = vscode.commands.registerCommand('game.机核网', () => {
 		news(context, 'gcores', '机核网', '机核网');
 	});
 
-	const 游民星空 = vscode.commands.registerCommand('extension.游民星空', () => {
+	const 游民星空 = vscode.commands.registerCommand('game.游民星空', () => {
 		news(context, 'gamersky', '游民星空', '游民星空');
 	});
 
-	const GameNews = vscode.commands.registerCommand('extension.GameNews', () => {
+	const GameNews = vscode.commands.registerCommand('game.GameNews', () => {
 		news(context, 'newslist', '游戏新闻', 'All');
 	});
 
